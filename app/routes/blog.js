ce1acb1d52dcec179d730c264695e1d239d195e6
andrew telleria and keir staple <keirstaple@comcast.net>
2016-03-31 16:21:55 -0700

    add comments to dynamically generated blog pages

--- app/routes/blog.js
+++ app/routes/blog.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.store.findRecord('blog', params.blog_id);
+    return this.store.findRecord('blog', params.blog_id)
   },
 
   actions: {
